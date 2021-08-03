68240c6959e3e6200b87c0b07ef4aa0441fa877a
danok21 <yd_40954@163.com>
2021-08-03 16:11:06 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,21 @@ async fn main() -> anyhow::Result<()> {
 
     /*
     let sql="INSERT INTO student (id,name,age,hobby)VALUES ( $1,$2,$3,$4 )";
-    let count=sqlx::query(sql).bind(4).bind("熊大").bind(7).bind("跑步").execute(&pool).await?;
+    let count=sqlx::query(sql).bind(4).bind("Alice").bind(7).bind("running").execute(&pool).await?;
     println!("{}",count);
     */
 
     /*
-    let sql=  "DELETE FROM student WHERE  hobby = '跑步'";
+    let sql=  "DELETE FROM student WHERE  hobby = 'running'";
     let count = sqlx::query(sql).execute(&pool).await?;
     println!("{}",count);
     */
 
-    /*
-    let sql=  "UPDATE student SET hobby = '打羽毛球和骑车' WHERE id = 2 ";
+
+    let sql=  "UPDATE student SET hobby = 'cycle' WHERE id = 2 ";
     let count=sqlx::query(sql).execute(&pool).await?;
     println!("{}",count);
-    */
+
 
     let sql = "SELECT * FROM student ORDER BY id";
     let mut cursor:PgCursor = sqlx::query(sql).fetch(&pool);
